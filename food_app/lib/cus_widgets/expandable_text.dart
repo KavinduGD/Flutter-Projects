41dce3eb5c37bb62013516f381c4ab0a4217baf5
KavinduGD <it21038914@my.sliit.lk>
2023-09-29 15:17:29 +0530

    4.19

---
+++ food_app/lib/cus_widgets/expandable_text.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/cus_widgets/small_text.dart';
+import 'package:food_app/utils/dimensions.dart';
+
+class ExpandableText extends StatefulWidget {
+  final String text;
+  const ExpandableText({required this.text, super.key});
+
+  @override
+  _ExpandableTextState createState() => _ExpandableTextState();
+}
+
+class _ExpandableTextState extends State<ExpandableText> {
+  late String firstHalf;
+  late String secondHalf;
+
+  bool hiddenText = true;
+
+  double textHeight = Dimensions.screenHeight / 5.63;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.text.length > textHeight) {
+      firstHalf = widget.text.substring(0, textHeight.toInt());
+      secondHalf =
+          widget.text.substring(textHeight.toInt() + 1, widget.text.length);
+    } else {
+      firstHalf = widget.text;
+      secondHalf = "";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: secondHalf.isEmpty
+          ? SmallText(
+              text: widget.text,
+            )
+          : Column(
+              children: [
+                SmallText(
+                  text: hiddenText
+                      ? (firstHalf + "...")
+                      : (firstHalf + secondHalf),
+                ),
+                InkWell(onTap: ,)
+              ],
+            ),
+    );
+  }
+}
